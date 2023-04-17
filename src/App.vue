6801e8e4bd9c7d39f1435b4610ebb5e92b2be54a
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2023-04-17 23:44:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+// local component registration
+//Local registration scopes the availability of the registered components
+//to the current component only. It makes the dependency relationship more explicit, and is more tree-shaking friendly.
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+import BadgeList from './components/BadgeList.vue';
+
+export default {
+  components: {
+    'the-header': TheHeader,
+    'user-info': UserInfo,
+    'badge-list': BadgeList
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>More on Vue Components</h1>
+  </header>
+</template>
+
+//When a style tag has the scoped attribute, its CSS will apply to elements of the current component only.
+<style scoped>
+  header {
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #14005e;
+  }
+
+  header h1 {
+    color: white;
+    margin: 0;
+  }
+</style>
